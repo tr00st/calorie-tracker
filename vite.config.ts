6828a12dcc4b8b2b675d36954cb3d60d1be0d4d0
James Cheese <trust@tr00st.co.uk>
2025-05-10 16:37:36 +0100

    Hooking linter up to vite logs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslintPlugin from '@nabla/vite-plugin-eslint';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), eslintPlugin()],
     base: '/calorie-tracker',
     resolve: {
         extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.d.ts'],
