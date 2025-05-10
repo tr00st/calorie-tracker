15b11f7964ac5845c8c73bae6e190453bd43da2b
James Cheese <trust@tr00st.co.uk>
2025-05-10 16:38:23 +0100

    Allow adding fixed-serving foods

--- src/components/add-food-dialog.tsx/AddFoodBody.tsx
+++ src/components/add-food-dialog.tsx/AddFoodBody.tsx
@@ -1,6 +1,7 @@
-import { Button, DialogActions, DialogContent, DialogTitle, Stack, TextField } from '@mui/material';
+import { Button, Collapse, DialogActions, DialogContent, DialogTitle, FormControl, InputLabel, MenuItem, Select, Stack, TextField } from '@mui/material';
 import { useState } from 'react';
 import { useSupabase } from '../../utils/supabase';
+import { FoodType } from '../../types/foods';
 
 const AddFoodBody = ({
     onClose,
@@ -13,6 +14,7 @@ const AddFoodBody = ({
     const [description, setDescription] = useState<string>('');
     const [calories, setCalories] = useState<string>('');
     const [servingSize, setServingSize] = useState<string>('100');
+    const [servingType, setServingType] = useState<FoodType>(FoodType.BY_WEIGHT);
     const caloriesRegex = /^[0-9]+$/;
     const isValidCalorieValue = (value: string) => {
         return caloriesRegex.test(value);
@@ -26,9 +28,11 @@ const AddFoodBody = ({
         const { error } = await supabase
             .from('foods')
             .insert({
-                calories_p100: calories,
                 name: name,
-                serving_grams: servingSize,
+                type: servingType,
+                calories_p100: servingType === FoodType.BY_WEIGHT ? calories : null,
+                serving_grams: servingType === FoodType.BY_WEIGHT ? servingSize : null,
+                calories_fixed: servingType === FoodType.FIXED_SERVING ? calories : null,
                 description: description,
             });
 
@@ -51,23 +55,53 @@ const AddFoodBody = ({
             </DialogTitle>
             <DialogContent sx={{ paddingTop: '1em!important' }}>
                 <Stack spacing={1}>
-                    <TextField
-                        label="Calories per 100g"
-                        autoFocus
-                        required
-                        value={calories}
-                        onChange={event => setCalories(event.target.value)}
-                        type="number"
-                        onKeyDown={handleSaveOnEnterKey}
-                    />
-                    <TextField
-                        label="Serving size (g)"
-                        required
-                        value={servingSize}
-                        onChange={event => setServingSize(event.target.value)}
-                        type="number"
-                        onKeyDown={handleSaveOnEnterKey}
-                    />
+                    <FormControl sx={{ m: 1, minWidth: 120 }}>
+                        <InputLabel id="serving-type-label">Serving Type</InputLabel>
+                        <Select
+                            label="Serving Type"
+                            labelId="serving-type-label"
+                            id="serving-type"
+                            value={servingType}
+                            onChange={event => setServingType(event.target.value as FoodType)}
+                        >
+                            <MenuItem value={FoodType.BY_WEIGHT}>By Weight</MenuItem>
+                            <MenuItem value={FoodType.FIXED_SERVING}>Fixed</MenuItem>
+                        </Select>
+                    </FormControl>
+                    <Collapse in={servingType === FoodType.BY_WEIGHT} unmountOnExit>
+                        <Stack direction="column" spacing={1} marginTop={1}>
+                            <TextField
+                                label="Calories per 100g"
+                                autoFocus
+                                required
+                                value={calories}
+                                onChange={event => setCalories(event.target.value)}
+                                type="number"
+                                onKeyDown={handleSaveOnEnterKey}
+                            />
+                            <TextField
+                                label="Serving size (g)"
+                                required
+                                value={servingSize}
+                                onChange={event => setServingSize(event.target.value)}
+                                type="number"
+                                onKeyDown={handleSaveOnEnterKey}
+                            />
+                        </Stack>
+                    </Collapse>
+                    <Collapse in={servingType === FoodType.FIXED_SERVING} unmountOnExit>
+                        <Stack direction="column" spacing={1} marginTop={1}>
+                            <TextField
+                                label="Calories per serving"
+                                autoFocus
+                                required
+                                value={calories}
+                                onChange={event => setCalories(event.target.value)}
+                                type="number"
+                                onKeyDown={handleSaveOnEnterKey}
+                            />
+                        </Stack>
+                    </Collapse>
                     <TextField
                         label="Name"
                         value={name}
